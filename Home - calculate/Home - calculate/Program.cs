36b8954f2d371dba417b4c7bafe22bdc17c2c301
John-Chernii <91661912+John-Chernii@users.noreply.github.com>
2021-10-25 20:34:20 +0300

    Add files via upload

---
+++ Home - calculate/Home - calculate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Home___calculate
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            int q = 0,
+                w = 0,
+                e = 0,
+                r = 0;
+            foreach (int l in str)
+            {
+                if (l == 'q')
+                {
+                    q++;
+                }
+                else if (l == 'w')
+                {
+                    w++;
+                }
+                else if (l == 'e')
+                {
+                    e++;
+                }
+                else if (l == 'r')
+                {
+                    r++;
+                }
+            }
+            Console.WriteLine($"q={q}\nw={w}\ne={e}\nr={r}");
+        }
+    }
+}
