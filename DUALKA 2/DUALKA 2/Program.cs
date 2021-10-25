0ceefb9837483081519b54827efa16eb5ce713eb
John-Chernii <91661912+John-Chernii@users.noreply.github.com>
2021-10-25 19:54:47 +0300

    Add files via upload

---
+++ DUALKA 2/DUALKA 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dual1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How are you?");
+            string mood = Console.ReadLine();
+            Console.WriteLine($"You are {mood} ");
+        }
+
+    }
+}
+
---
+++ DUALKA 4/DUALKA 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DUALKA_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Enter X = ");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Enter Y = ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{X}, {Y}, {(X > 0 && Y > 0)}");
+        }
+    }
+}
